beafcb9528013e7aff0ee627f7fde7947d5b8c50
Daniela Posas <daniela_mnl_92@hotmail.com>
2016-03-21 09:28:59 +0100

    transiciones sin to_String, he dejado los numeros estáticos, antes pulsar izquierda y cambiaban los numeros, pero creo que no servia de mucho. (Pd: En el hud al pulsar 1 cambia el personaje principal, izquierda-derecha cambia el nivel de vida del principal predefinido)

--- transicionPresidentum/build/Debug/MinGW-Windows/main.o.d
+++ transicionPresidentum/build/Debug/MinGW-Windows/main.o.d
@@ -1,259 +1,254 @@
 build/Debug/MinGW-Windows/main.o: main.cpp \
- ../../../../../SFML/include/SFML/Graphics.hpp \
- ../../../../../SFML/include/SFML/Window.hpp \
- ../../../../../SFML/include/SFML/System.hpp \
- ../../../../../SFML/include/SFML/Config.hpp \
- ../../../../../SFML/include/SFML/System/Clock.hpp \
- ../../../../../SFML/include/SFML/System/Export.hpp \
- ../../../../../SFML/include/SFML/System/Time.hpp \
- ../../../../../SFML/include/SFML/System/Err.hpp \
- ../../../../../SFML/include/SFML/System/FileInputStream.hpp \
- ../../../../../SFML/include/SFML/System/InputStream.hpp \
- ../../../../../SFML/include/SFML/System/NonCopyable.hpp \
- ../../../../../SFML/include/SFML/System/Lock.hpp \
- ../../../../../SFML/include/SFML/System/MemoryInputStream.hpp \
- ../../../../../SFML/include/SFML/System/Mutex.hpp \
- ../../../../../SFML/include/SFML/System/Sleep.hpp \
- ../../../../../SFML/include/SFML/System/String.hpp \
- ../../../../../SFML/include/SFML/System/Utf.hpp \
- ../../../../../SFML/include/SFML/System/Utf.inl \
- ../../../../../SFML/include/SFML/System/String.inl \
- ../../../../../SFML/include/SFML/System/Thread.hpp \
- ../../../../../SFML/include/SFML/System/Thread.inl \
- ../../../../../SFML/include/SFML/System/ThreadLocal.hpp \
- ../../../../../SFML/include/SFML/System/ThreadLocalPtr.hpp \
- ../../../../../SFML/include/SFML/System/ThreadLocalPtr.inl \
- ../../../../../SFML/include/SFML/System/Vector2.hpp \
- ../../../../../SFML/include/SFML/System/Vector2.inl \
- ../../../../../SFML/include/SFML/System/Vector3.hpp \
- ../../../../../SFML/include/SFML/System/Vector3.inl \
- ../../../../../SFML/include/SFML/Window/Context.hpp \
- ../../../../../SFML/include/SFML/Window/Export.hpp \
- ../../../../../SFML/include/SFML/Window/GlResource.hpp \
- ../../../../../SFML/include/SFML/Window/ContextSettings.hpp \
- ../../../../../SFML/include/SFML/Window/Event.hpp \
- ../../../../../SFML/include/SFML/Window/Joystick.hpp \
- ../../../../../SFML/include/SFML/Window/Keyboard.hpp \
- ../../../../../SFML/include/SFML/Window/Mouse.hpp \
- ../../../../../SFML/include/SFML/Window/Sensor.hpp \
- ../../../../../SFML/include/SFML/Window/Touch.hpp \
- ../../../../../SFML/include/SFML/Window/VideoMode.hpp \
- ../../../../../SFML/include/SFML/Window/Window.hpp \
- ../../../../../SFML/include/SFML/Window/WindowHandle.hpp \
- ../../../../../SFML/include/SFML/Window/WindowStyle.hpp \
- ../../../../../SFML/include/SFML/Graphics/BlendMode.hpp \
- ../../../../../SFML/include/SFML/Graphics/Export.hpp \
- ../../../../../SFML/include/SFML/Graphics/CircleShape.hpp \
- ../../../../../SFML/include/SFML/Graphics/Shape.hpp \
- ../../../../../SFML/include/SFML/Graphics/Drawable.hpp \
- ../../../../../SFML/include/SFML/Graphics/RenderStates.hpp \
- ../../../../../SFML/include/SFML/Graphics/Transform.hpp \
- ../../../../../SFML/include/SFML/Graphics/Rect.hpp \
- ../../../../../SFML/include/SFML/Graphics/Rect.inl \
- ../../../../../SFML/include/SFML/Graphics/Transformable.hpp \
- ../../../../../SFML/include/SFML/Graphics/VertexArray.hpp \
- ../../../../../SFML/include/SFML/Graphics/Vertex.hpp \
- ../../../../../SFML/include/SFML/Graphics/Color.hpp \
- ../../../../../SFML/include/SFML/Graphics/PrimitiveType.hpp \
- ../../../../../SFML/include/SFML/Graphics/ConvexShape.hpp \
- ../../../../../SFML/include/SFML/Graphics/Font.hpp \
- ../../../../../SFML/include/SFML/Graphics/Glyph.hpp \
- ../../../../../SFML/include/SFML/Graphics/Texture.hpp \
- ../../../../../SFML/include/SFML/Graphics/Image.hpp \
- ../../../../../SFML/include/SFML/Graphics/RectangleShape.hpp \
- ../../../../../SFML/include/SFML/Graphics/RenderTarget.hpp \
- ../../../../../SFML/include/SFML/Graphics/View.hpp \
- ../../../../../SFML/include/SFML/Graphics/RenderTexture.hpp \
- ../../../../../SFML/include/SFML/Graphics/RenderWindow.hpp \
- ../../../../../SFML/include/SFML/Graphics/Shader.hpp \
- ../../../../../SFML/include/SFML/Graphics/Sprite.hpp \
- ../../../../../SFML/include/SFML/Graphics/Text.hpp \
- ../../../../../SFML/include/SFML/Audio.hpp \
- ../../../../../SFML/include/SFML/Audio/InputSoundFile.hpp \
- ../../../../../SFML/include/SFML/Audio/Export.hpp \
- ../../../../../SFML/include/SFML/Audio/Listener.hpp \
- ../../../../../SFML/include/SFML/Audio/Music.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundStream.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundSource.hpp \
- ../../../../../SFML/include/SFML/Audio/AlResource.hpp \
- ../../../../../SFML/include/SFML/Audio/OutputSoundFile.hpp \
- ../../../../../SFML/include/SFML/Audio/Sound.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundBuffer.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundBufferRecorder.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundRecorder.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundFileFactory.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundFileFactory.inl \
- ../../../../../SFML/include/SFML/Audio/SoundFileReader.hpp \
- ../../../../../SFML/include/SFML/Audio/SoundFileWriter.hpp
+ c:/SFML/include/SFML/Graphics.hpp c:/SFML/include/SFML/Window.hpp \
+ c:/SFML/include/SFML/System.hpp c:/SFML/include/SFML/Config.hpp \
+ c:/SFML/include/SFML/System/Clock.hpp \
+ c:/SFML/include/SFML/System/Export.hpp \
+ c:/SFML/include/SFML/System/Time.hpp c:/SFML/include/SFML/System/Err.hpp \
+ c:/SFML/include/SFML/System/FileInputStream.hpp \
+ c:/SFML/include/SFML/System/InputStream.hpp \
+ c:/SFML/include/SFML/System/NonCopyable.hpp \
+ c:/SFML/include/SFML/System/Lock.hpp \
+ c:/SFML/include/SFML/System/MemoryInputStream.hpp \
+ c:/SFML/include/SFML/System/Mutex.hpp \
+ c:/SFML/include/SFML/System/Sleep.hpp \
+ c:/SFML/include/SFML/System/String.hpp \
+ c:/SFML/include/SFML/System/Utf.hpp c:/SFML/include/SFML/System/Utf.inl \
+ c:/SFML/include/SFML/System/String.inl \
+ c:/SFML/include/SFML/System/Thread.hpp \
+ c:/SFML/include/SFML/System/Thread.inl \
+ c:/SFML/include/SFML/System/ThreadLocal.hpp \
+ c:/SFML/include/SFML/System/ThreadLocalPtr.hpp \
+ c:/SFML/include/SFML/System/ThreadLocalPtr.inl \
+ c:/SFML/include/SFML/System/Vector2.hpp \
+ c:/SFML/include/SFML/System/Vector2.inl \
+ c:/SFML/include/SFML/System/Vector3.hpp \
+ c:/SFML/include/SFML/System/Vector3.inl \
+ c:/SFML/include/SFML/Window/Context.hpp \
+ c:/SFML/include/SFML/Window/Export.hpp \
+ c:/SFML/include/SFML/Window/GlResource.hpp \
+ c:/SFML/include/SFML/Window/ContextSettings.hpp \
+ c:/SFML/include/SFML/Window/Event.hpp \
+ c:/SFML/include/SFML/Window/Joystick.hpp \
+ c:/SFML/include/SFML/Window/Keyboard.hpp \
+ c:/SFML/include/SFML/Window/Mouse.hpp \
+ c:/SFML/include/SFML/Window/Sensor.hpp \
+ c:/SFML/include/SFML/Window/Touch.hpp \
+ c:/SFML/include/SFML/Window/VideoMode.hpp \
+ c:/SFML/include/SFML/Window/Window.hpp \
+ c:/SFML/include/SFML/Window/WindowHandle.hpp \
+ c:/SFML/include/SFML/Window/WindowStyle.hpp \
+ c:/SFML/include/SFML/Graphics/BlendMode.hpp \
+ c:/SFML/include/SFML/Graphics/Export.hpp \
+ c:/SFML/include/SFML/Graphics/CircleShape.hpp \
+ c:/SFML/include/SFML/Graphics/Shape.hpp \
+ c:/SFML/include/SFML/Graphics/Drawable.hpp \
+ c:/SFML/include/SFML/Graphics/RenderStates.hpp \
+ c:/SFML/include/SFML/Graphics/Transform.hpp \
+ c:/SFML/include/SFML/Graphics/Rect.hpp \
+ c:/SFML/include/SFML/Graphics/Rect.inl \
+ c:/SFML/include/SFML/Graphics/Transformable.hpp \
+ c:/SFML/include/SFML/Graphics/VertexArray.hpp \
+ c:/SFML/include/SFML/Graphics/Vertex.hpp \
+ c:/SFML/include/SFML/Graphics/Color.hpp \
+ c:/SFML/include/SFML/Graphics/PrimitiveType.hpp \
+ c:/SFML/include/SFML/Graphics/ConvexShape.hpp \
+ c:/SFML/include/SFML/Graphics/Font.hpp \
+ c:/SFML/include/SFML/Graphics/Glyph.hpp \
+ c:/SFML/include/SFML/Graphics/Texture.hpp \
+ c:/SFML/include/SFML/Graphics/Image.hpp \
+ c:/SFML/include/SFML/Graphics/RectangleShape.hpp \
+ c:/SFML/include/SFML/Graphics/RenderTarget.hpp \
+ c:/SFML/include/SFML/Graphics/View.hpp \
+ c:/SFML/include/SFML/Graphics/RenderTexture.hpp \
+ c:/SFML/include/SFML/Graphics/RenderWindow.hpp \
+ c:/SFML/include/SFML/Graphics/Shader.hpp \
+ c:/SFML/include/SFML/Graphics/Sprite.hpp \
+ c:/SFML/include/SFML/Graphics/Text.hpp c:/SFML/include/SFML/Audio.hpp \
+ c:/SFML/include/SFML/Audio/InputSoundFile.hpp \
+ c:/SFML/include/SFML/Audio/Export.hpp \
+ c:/SFML/include/SFML/Audio/Listener.hpp \
+ c:/SFML/include/SFML/Audio/Music.hpp \
+ c:/SFML/include/SFML/Audio/SoundStream.hpp \
+ c:/SFML/include/SFML/Audio/SoundSource.hpp \
+ c:/SFML/include/SFML/Audio/AlResource.hpp \
+ c:/SFML/include/SFML/Audio/OutputSoundFile.hpp \
+ c:/SFML/include/SFML/Audio/Sound.hpp \
+ c:/SFML/include/SFML/Audio/SoundBuffer.hpp \
+ c:/SFML/include/SFML/Audio/SoundBufferRecorder.hpp \
+ c:/SFML/include/SFML/Audio/SoundRecorder.hpp \
+ c:/SFML/include/SFML/Audio/SoundFileFactory.hpp \
+ c:/SFML/include/SFML/Audio/SoundFileFactory.inl \
+ c:/SFML/include/SFML/Audio/SoundFileReader.hpp \
+ c:/SFML/include/SFML/Audio/SoundFileWriter.hpp
 
-../../../../../SFML/include/SFML/Graphics.hpp:
+c:/SFML/include/SFML/Graphics.hpp:
 
-../../../../../SFML/include/SFML/Window.hpp:
+c:/SFML/include/SFML/Window.hpp:
 
-../../../../../SFML/include/SFML/System.hpp:
+c:/SFML/include/SFML/System.hpp:
 
-../../../../../SFML/include/SFML/Config.hpp:
+c:/SFML/include/SFML/Config.hpp:
 
-../../../../../SFML/include/SFML/System/Clock.hpp:
+c:/SFML/include/SFML/System/Clock.hpp:
 
-../../../../../SFML/include/SFML/System/Export.hpp:
+c:/SFML/include/SFML/System/Export.hpp:
 
-../../../../../SFML/include/SFML/System/Time.hpp:
+c:/SFML/include/SFML/System/Time.hpp:
 
-../../../../../SFML/include/SFML/System/Err.hpp:
+c:/SFML/include/SFML/System/Err.hpp:
 
-../../../../../SFML/include/SFML/System/FileInputStream.hpp:
+c:/SFML/include/SFML/System/FileInputStream.hpp:
 
-../../../../../SFML/include/SFML/System/InputStream.hpp:
+c:/SFML/include/SFML/System/InputStream.hpp:
 
-../../../../../SFML/include/SFML/System/NonCopyable.hpp:
+c:/SFML/include/SFML/System/NonCopyable.hpp:
 
-../../../../../SFML/include/SFML/System/Lock.hpp:
+c:/SFML/include/SFML/System/Lock.hpp:
 
-../../../../../SFML/include/SFML/System/MemoryInputStream.hpp:
+c:/SFML/include/SFML/System/MemoryInputStream.hpp:
 
-../../../../../SFML/include/SFML/System/Mutex.hpp:
+c:/SFML/include/SFML/System/Mutex.hpp:
 
-../../../../../SFML/include/SFML/System/Sleep.hpp:
+c:/SFML/include/SFML/System/Sleep.hpp:
 
-../../../../../SFML/include/SFML/System/String.hpp:
+c:/SFML/include/SFML/System/String.hpp:
 
-../../../../../SFML/include/SFML/System/Utf.hpp:
+c:/SFML/include/SFML/System/Utf.hpp:
 
-../../../../../SFML/include/SFML/System/Utf.inl:
+c:/SFML/include/SFML/System/Utf.inl:
 
-../../../../../SFML/include/SFML/System/String.inl:
+c:/SFML/include/SFML/System/String.inl:
 
-../../../../../SFML/include/SFML/System/Thread.hpp:
+c:/SFML/include/SFML/System/Thread.hpp:
 
-../../../../../SFML/include/SFML/System/Thread.inl:
+c:/SFML/include/SFML/System/Thread.inl:
 
-../../../../../SFML/include/SFML/System/ThreadLocal.hpp:
+c:/SFML/include/SFML/System/ThreadLocal.hpp:
 
-../../../../../SFML/include/SFML/System/ThreadLocalPtr.hpp:
+c:/SFML/include/SFML/System/ThreadLocalPtr.hpp:
 
-../../../../../SFML/include/SFML/System/ThreadLocalPtr.inl:
+c:/SFML/include/SFML/System/ThreadLocalPtr.inl:
 
-../../../../../SFML/include/SFML/System/Vector2.hpp:
+c:/SFML/include/SFML/System/Vector2.hpp:
 
-../../../../../SFML/include/SFML/System/Vector2.inl:
+c:/SFML/include/SFML/System/Vector2.inl:
 
-../../../../../SFML/include/SFML/System/Vector3.hpp:
+c:/SFML/include/SFML/System/Vector3.hpp:
 
-../../../../../SFML/include/SFML/System/Vector3.inl:
+c:/SFML/include/SFML/System/Vector3.inl:
 
-../../../../../SFML/include/SFML/Window/Context.hpp:
+c:/SFML/include/SFML/Window/Context.hpp:
 
-../../../../../SFML/include/SFML/Window/Export.hpp:
+c:/SFML/include/SFML/Window/Export.hpp:
 
-../../../../../SFML/include/SFML/Window/GlResource.hpp:
+c:/SFML/include/SFML/Window/GlResource.hpp:
 
-../../../../../SFML/include/SFML/Window/ContextSettings.hpp:
+c:/SFML/include/SFML/Window/ContextSettings.hpp:
 
-../../../../../SFML/include/SFML/Window/Event.hpp:
+c:/SFML/include/SFML/Window/Event.hpp:
 
-../../../../../SFML/include/SFML/Window/Joystick.hpp:
+c:/SFML/include/SFML/Window/Joystick.hpp:
 
-../../../../../SFML/include/SFML/Window/Keyboard.hpp:
+c:/SFML/include/SFML/Window/Keyboard.hpp:
 
-../../../../../SFML/include/SFML/Window/Mouse.hpp:
+c:/SFML/include/SFML/Window/Mouse.hpp:
 
-../../../../../SFML/include/SFML/Window/Sensor.hpp:
+c:/SFML/include/SFML/Window/Sensor.hpp:
 
-../../../../../SFML/include/SFML/Window/Touch.hpp:
+c:/SFML/include/SFML/Window/Touch.hpp:
 
-../../../../../SFML/include/SFML/Window/VideoMode.hpp:
+c:/SFML/include/SFML/Window/VideoMode.hpp:
 
-../../../../../SFML/include/SFML/Window/Window.hpp:
+c:/SFML/include/SFML/Window/Window.hpp:
 
-../../../../../SFML/include/SFML/Window/WindowHandle.hpp:
+c:/SFML/include/SFML/Window/WindowHandle.hpp:
 
-../../../../../SFML/include/SFML/Window/WindowStyle.hpp:
+c:/SFML/include/SFML/Window/WindowStyle.hpp:
 
-../../../../../SFML/include/SFML/Graphics/BlendMode.hpp:
+c:/SFML/include/SFML/Graphics/BlendMode.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Export.hpp:
+c:/SFML/include/SFML/Graphics/Export.hpp:
 
-../../../../../SFML/include/SFML/Graphics/CircleShape.hpp:
+c:/SFML/include/SFML/Graphics/CircleShape.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Shape.hpp:
+c:/SFML/include/SFML/Graphics/Shape.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Drawable.hpp:
+c:/SFML/include/SFML/Graphics/Drawable.hpp:
 
-../../../../../SFML/include/SFML/Graphics/RenderStates.hpp:
+c:/SFML/include/SFML/Graphics/RenderStates.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Transform.hpp:
+c:/SFML/include/SFML/Graphics/Transform.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Rect.hpp:
+c:/SFML/include/SFML/Graphics/Rect.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Rect.inl:
+c:/SFML/include/SFML/Graphics/Rect.inl:
 
-../../../../../SFML/include/SFML/Graphics/Transformable.hpp:
+c:/SFML/include/SFML/Graphics/Transformable.hpp:
 
-../../../../../SFML/include/SFML/Graphics/VertexArray.hpp:
+c:/SFML/include/SFML/Graphics/VertexArray.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Vertex.hpp:
+c:/SFML/include/SFML/Graphics/Vertex.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Color.hpp:
+c:/SFML/include/SFML/Graphics/Color.hpp:
 
-../../../../../SFML/include/SFML/Graphics/PrimitiveType.hpp:
+c:/SFML/include/SFML/Graphics/PrimitiveType.hpp:
 
-../../../../../SFML/include/SFML/Graphics/ConvexShape.hpp:
+c:/SFML/include/SFML/Graphics/ConvexShape.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Font.hpp:
+c:/SFML/include/SFML/Graphics/Font.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Glyph.hpp:
+c:/SFML/include/SFML/Graphics/Glyph.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Texture.hpp:
+c:/SFML/include/SFML/Graphics/Texture.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Image.hpp:
+c:/SFML/include/SFML/Graphics/Image.hpp:
 
-../../../../../SFML/include/SFML/Graphics/RectangleShape.hpp:
+c:/SFML/include/SFML/Graphics/RectangleShape.hpp:
 
-../../../../../SFML/include/SFML/Graphics/RenderTarget.hpp:
+c:/SFML/include/SFML/Graphics/RenderTarget.hpp:
 
-../../../../../SFML/include/SFML/Graphics/View.hpp:
+c:/SFML/include/SFML/Graphics/View.hpp:
 
-../../../../../SFML/include/SFML/Graphics/RenderTexture.hpp:
+c:/SFML/include/SFML/Graphics/RenderTexture.hpp:
 
-../../../../../SFML/include/SFML/Graphics/RenderWindow.hpp:
+c:/SFML/include/SFML/Graphics/RenderWindow.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Shader.hpp:
+c:/SFML/include/SFML/Graphics/Shader.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Sprite.hpp:
+c:/SFML/include/SFML/Graphics/Sprite.hpp:
 
-../../../../../SFML/include/SFML/Graphics/Text.hpp:
+c:/SFML/include/SFML/Graphics/Text.hpp:
 
-../../../../../SFML/include/SFML/Audio.hpp:
+c:/SFML/include/SFML/Audio.hpp:
 
-../../../../../SFML/include/SFML/Audio/InputSoundFile.hpp:
+c:/SFML/include/SFML/Audio/InputSoundFile.hpp:
 
-../../../../../SFML/include/SFML/Audio/Export.hpp:
+c:/SFML/include/SFML/Audio/Export.hpp:
 
-../../../../../SFML/include/SFML/Audio/Listener.hpp:
+c:/SFML/include/SFML/Audio/Listener.hpp:
 
-../../../../../SFML/include/SFML/Audio/Music.hpp:
+c:/SFML/include/SFML/Audio/Music.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundStream.hpp:
+c:/SFML/include/SFML/Audio/SoundStream.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundSource.hpp:
+c:/SFML/include/SFML/Audio/SoundSource.hpp:
 
-../../../../../SFML/include/SFML/Audio/AlResource.hpp:
+c:/SFML/include/SFML/Audio/AlResource.hpp:
 
-../../../../../SFML/include/SFML/Audio/OutputSoundFile.hpp:
+c:/SFML/include/SFML/Audio/OutputSoundFile.hpp:
 
-../../../../../SFML/include/SFML/Audio/Sound.hpp:
+c:/SFML/include/SFML/Audio/Sound.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundBuffer.hpp:
+c:/SFML/include/SFML/Audio/SoundBuffer.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundBufferRecorder.hpp:
+c:/SFML/include/SFML/Audio/SoundBufferRecorder.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundRecorder.hpp:
+c:/SFML/include/SFML/Audio/SoundRecorder.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundFileFactory.hpp:
+c:/SFML/include/SFML/Audio/SoundFileFactory.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundFileFactory.inl:
+c:/SFML/include/SFML/Audio/SoundFileFactory.inl:
 
-../../../../../SFML/include/SFML/Audio/SoundFileReader.hpp:
+c:/SFML/include/SFML/Audio/SoundFileReader.hpp:
 
-../../../../../SFML/include/SFML/Audio/SoundFileWriter.hpp:
+c:/SFML/include/SFML/Audio/SoundFileWriter.hpp:
--- transicionPresidentum/main.cpp
+++ transicionPresidentum/main.cpp
@@ -18,7 +18,7 @@
  #include <fstream>
  #include <cstring>
 
-#define kVel 5
+
 using namespace std;
 using namespace sf;
 
@@ -121,7 +121,7 @@ int main()
     
      //Cargo la imagen donde reside la textura del sprite
     sf::Texture texfondo;
-    if (!texfondo.loadFromFile("resources/fondo.jpg"))
+    if (!texfondo.loadFromFile("resources/fondo.png"))
     {
         std::cerr << "Error cargando la imagen caras.jpg";
         exit(0);
@@ -129,9 +129,84 @@ int main()
     
     sf::Sprite spritefondo(texfondo);
     
-    spritefondo.setOrigin(matriz[0][0]/2,matriz[0][0]/2);
-    spritefondo.setTextureRect(sf::IntRect(matriz[0][0], matriz[0][1], matriz[0][2], matriz[0][3]));
+    spritefondo.setOrigin(matriz[2][0]/2,matriz[2][0]/2);
+    spritefondo.setTextureRect(sf::IntRect(matriz[2][0], matriz[2][1], matriz[2][2], matriz[2][3]));
     spritefondo.setPosition(0,0);
+    
+    
+     //Cargo la imagen donde reside la textura del sprite
+    sf::Texture texpresi;
+    if (!texpresi.loadFromFile("resources/fondo.png"))
+    {
+        std::cerr << "Error cargando la imagen caras.jpg";
+        exit(0);
+    }
+    
+    sf::Sprite spritepresi(texpresi);
+    
+    spritepresi.setOrigin(matriz[4][2]/2,matriz[4][3]/2);
+    spritepresi.setTextureRect(sf::IntRect(matriz[4][0], matriz[4][1], matriz[4][2], matriz[4][3]));
+    spritepresi.setPosition(500,80);
+    
+    
+     //Cargo la imagen donde reside la textura del sprite
+    sf::Texture textutorial;
+    if (!textutorial.loadFromFile("resources/fondo.png"))
+    {
+        std::cerr << "Error cargando la imagen caras.jpg";
+        exit(0);
+    }
+    
+    sf::Sprite spritetutorial(textutorial);
+    
+    spritetutorial.setOrigin(matriz[5][2]/2,matriz[5][3]/2);
+    spritetutorial.setTextureRect(sf::IntRect(matriz[5][0], matriz[5][1], matriz[5][2], matriz[5][3]));
+    spritetutorial.setPosition(500,80);
+    
+    
+    
+     //Cargo la imagen donde reside la textura del sprite
+    sf::Texture texnivel;
+    if (!texnivel.loadFromFile("resources/fondo.png"))
+    {
+        std::cerr << "Error cargando la imagen caras.jpg";
+        exit(0);
+    }
+    
+    sf::Sprite spritenivel(texnivel);
+    
+    spritenivel.setOrigin(matriz[3][2]/2,matriz[3][3]/2);
+    spritenivel.setTextureRect(sf::IntRect(matriz[3][0], matriz[3][1], matriz[3][2], matriz[3][3]));
+    spritenivel.setPosition(500,80);
+    
+    
+     //Cargo la imagen donde reside la textura del sprite
+    sf::Texture texenhorabuena;
+    if (!texenhorabuena.loadFromFile("resources/fondo.png"))
+    {
+        std::cerr << "Error cargando la imagen caras.jpg";
+        exit(0);
+    }
+    
+    sf::Sprite spriteenhorabuena(texenhorabuena);
+    
+    spriteenhorabuena.setOrigin(matriz[0][2]/2,matriz[0][3]/2);
+    spriteenhorabuena.setTextureRect(sf::IntRect(matriz[0][0], matriz[0][1], matriz[0][2], matriz[0][3]));
+    spriteenhorabuena.setPosition(500,80);
+    
+     //Cargo la imagen donde reside la textura del sprite
+    sf::Texture texfelicidades;
+    if (!texfelicidades.loadFromFile("resources/fondo.png"))
+    {
+        std::cerr << "Error cargando la imagen caras.jpg";
+        exit(0);
+    }
+    
+    sf::Sprite spritefelicidades(texfelicidades);
+    
+    spritefelicidades.setOrigin(matriz[1][2]/2,matriz[1][3]/2);
+    spritefelicidades.setTextureRect(sf::IntRect(matriz[1][0], matriz[1][1], matriz[1][2], matriz[1][3]));
+    spritefelicidades.setPosition(500,80);
     
     
      sf::Font font;  
@@ -152,7 +227,7 @@ int main()
      "\n""con una gran tasa de paro, muchos casos de corrupcion, etc.""\n""\n""\n"" Tu objetivo es que tu partido llegue a presidir el gobierno." "\n" "Para ello deberas superar los distintos niveles derrotando a tus enemigos y consiguiendo el mayor numero de votos."
      "\n""Ademas, al final de cada nivel deberas enfrentarte a un jefe final y si lo deseas podras pactar con el.""\n""\n""\n""Al derrotar todos los jefes te convertiras en el presidente del pais." "\n" "Apostaras por el bipartidismo "
      "o por las nuevas fuerzas?""\n""EL PAIS TE NECESITA Y SOLO CON TU AYUDA SE PODRA ACABAR CON ESTA LOCURA.", fontpixelated, 50);
-     trans1.setPosition(10,100);
+     trans1.setPosition(10,200);
      trans1.setCharacterSize(20);
      trans1.setColor(sf::Color::Black);
      
@@ -165,36 +240,25 @@ int main()
      
      int n=1;
      
-     sf::Text trans3("Te encuentras en el nivel:", fontpixelated, 50);
+     sf::Text trans3("Te encuentras en el nivel: 2", fontpixelated, 50);
      trans3.setPosition(100,200);
      trans3.setCharacterSize(22);
      trans3.setColor(sf::Color::Black);
      
-    std::string countdownString;
-    std::ostringstream convert;
-    convert << n;
-    countdownString = convert.str();
+   
     
-     sf::Text trans31;
-     trans31.setFont(font);
-     trans31.setString(countdownString);
-     trans31.setPosition(350,200);
-     trans31.setCharacterSize(22);
-     trans31.setColor(sf::Color::Black);
      
-     
-     
-     sf::Text trans4("ENHORABUENA. Has superado el nivel""\n" "\n""VOTOS CONSEGUIDOS: 28""\n""\n"
+     sf::Text trans4("Has superado el nivel""\n" "\n""VOTOS CONSEGUIDOS: 28""\n""\n"
      "PACTO: NO REALIZADO""\n""\n""ENEMIGOS DERROTADOS: 3""\n""\n""SIMPATIZANTE LIBERADO: SI""\n"
      "\n""TIEMPO: 2m 30s" , fontpixelated, 50);
-     trans4.setPosition(50,100);
+     trans4.setPosition(50,200);
      trans4.setCharacterSize(22);
      trans4.setColor(sf::Color::Black);
      
      
-     sf::Text trans5("FELICIDADES, ERES EL NUEVO PRESIDENTE""\n""\n""VOTOS CONSEGUIDOS: 28""\n""\n""PACTOS REALIZADOS:3"
+     sf::Text trans5("ERES EL NUEVO PRESIDENTE""\n""\n""VOTOS CONSEGUIDOS: 28""\n""\n""PACTOS REALIZADOS:3"
      "\n""\n""ENEMIGOS DERROTADOS: 15""\n""\n""SIMPATIZANTES LIBERADOS:4""\n""\n""TIEMPO: 3m 35s""\n""\n""Llegan nuevos tiempo para el pais,""\n""CIUDADANOS ha llegado al poder y se presagian grandes cambios en la sociedad.""\n""Se respiran nuevos aires.", fontpixelated, 50);
-     trans5.setPosition(50,100);
+     trans5.setPosition(50,180);
      trans5.setCharacterSize(22);
      trans5.setColor(sf::Color::Black);
  
@@ -221,8 +285,7 @@ int main()
         
         while (window.pollEvent(event))
         {
-               trans31.setString( std::to_string(n));
-                
+            
                 
                 switch(event.type){
                 
@@ -253,12 +316,7 @@ int main()
                            
                             break;
                         
-                        case sf::Keyboard::Right:
-                               std::cerr << "entra";
-                                trans31.setString( std::to_string(n));
-                            n++; //subir nivel, para probar que cambia
-                            
-                            break;
+                   
                             
                         case sf::Keyboard::Num4:
                             i=4;
@@ -291,12 +349,17 @@ int main()
         
         window.draw(spritefondo);
         if(i==0)window.draw(opciones);
-        if(i==1) window.draw(trans1);  
-        if(i==2)  window.draw(trans2);   
-        if(i==3) { window.draw(trans3);
-         window.draw(trans31);}
-        if(i==4)  window.draw(trans4);
-        if(i==5)  window.draw(trans5);
+        if(i==1){ window.draw(trans1); 
+        window.draw(spritepresi);}
+        if(i==2){ window.draw(trans2);
+        window.draw(spritetutorial);} ;
+        
+        if(i==3){ window.draw(trans3);
+        window.draw(spritenivel);}
+        if(i==4) { window.draw(trans4);
+        window.draw(spriteenhorabuena);}
+        if(i==5) { window.draw(trans5);
+        window.draw(spritefelicidades);}
        
 
         
