b538347c3979ca41adb37592fa711dfbc3bcc33d
Aitor <Showzen@hotmail.es>
2016-05-03 13:20:49 +0200

    Cosas que faltaban del nivel1PP, ahora debería de funcionar.
    Sería interesante juntarlo todo aquí para la demo.

---
+++ Nivel1PP/build/Debug/MinGW-Windows/tinyxml.o.d
@@ -0,0 +1,5 @@
+build/Debug/MinGW-Windows/tinyxml.o: tinyxml.cpp tinyxml.h tinystr.h
+
+tinyxml.h:
+
+tinystr.h:
---
+++ Nivel1PP/build/Debug/MinGW-Windows/tinyxmlparser.o.d
@@ -0,0 +1,6 @@
+build/Debug/MinGW-Windows/tinyxmlparser.o: tinyxmlparser.cpp tinyxml.h \
+ tinystr.h
+
+tinyxml.h:
+
+tinystr.h:
